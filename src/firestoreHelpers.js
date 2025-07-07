2c374a22d0c4c6a6e7852b24a2e93e3c4b3575a2
Doskipo <eudaldborrell8@gmail.com>
2025-07-08 01:52:46 +0200

    Ususaris afegits correctament. Falten certes verificacions, pero funcional

--- src/firestoreHelpers.js
+++ src/firestoreHelpers.js
@@ -1,35 +1,35 @@
 import { db } from "./firebase";
-import { collection, getDocs, query, where } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
 
 /**
- * Llegeix totes les despeses
+ * Llegeix totes les despeses de l'usuari
  */
-export const getAllExpenses = async () => {
-  const snapshot = await getDocs(collection(db, "expenses"));
+export const getAllExpenses = async (uid) => {
+  const snapshot = await getDocs(collection(db, `users/${uid}/expenses`));
   return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
 };
 
 /**
- * Llegeix totes les categories
+ * Llegeix totes les categories de l'usuari
  */
-export const getAllCategories = async () => {
-  const snapshot = await getDocs(collection(db, "categories"));
+export const getAllCategories = async (uid) => {
+  const snapshot = await getDocs(collection(db, `users/${uid}/categories`));
   return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
 };
 
 /**
- * Llegeix tots els guanys
+ * Llegeix tots els guanys de l'usuari
  */
-export const getAllEarnings = async () => {
-  const snapshot = await getDocs(collection(db, "earnings"));
+export const getAllEarnings = async (uid) => {
+  const snapshot = await getDocs(collection(db, `users/${uid}/earnings`));
   return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
 };
 
 /**
- * Llegeix despeses per any i mes (format: YYYY i MM)
+ * Llegeix despeses per any i mes de l'usuari
  */
-export const getExpensesByMonth = async (year, month) => {
-  const all = await getAllExpenses();
+export const getExpensesByMonth = async (uid, year, month) => {
+  const all = await getAllExpenses(uid);
   return all.filter(e => {
     const d = new Date(e.date);
     return d.getFullYear() === year && d.getMonth() === month;
@@ -37,10 +37,10 @@ export const getExpensesByMonth = async (year, month) => {
 };
 
 /**
- * Llegeix guanys per any i mes
+ * Llegeix guanys per any i mes de l'usuari
  */
-export const getEarningsByMonth = async (year, month) => {
-  const all = await getAllEarnings();
+export const getEarningsByMonth = async (uid, year, month) => {
+  const all = await getAllEarnings(uid);
   return all.filter(e => {
     const d = new Date(e.date);
     return d.getFullYear() === year && d.getMonth() === month;

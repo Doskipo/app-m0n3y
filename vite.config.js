d44c35a1a736653d10b3813a780527042cb304ff
Doskipo <eudaldborrell8@gmail.com>
2025-07-09 20:47:36 +0200

    Canvis interns (manifest i index) - 2

--- vite.config.js
+++ vite.config.js
@@ -7,24 +7,29 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'favicon.ico', 'apple-touch-icon.png'],
+      includeAssets: [
+        'favicon.ico',
+        'apple-touch-icon.png',
+        'icons/Icon_Necse_WhiteTheme_192x192.png',
+        'icons/Icon_Necse_WhiteTheme_512x512.png'
+      ],
       manifest: {
-        name: 'M0N3Y',
-        short_name: 'M0N3Y',
+        name: 'NECSE',
+        short_name: 'NECSE',
         description: 'App de finances personals',
         theme_color: '#ffffff',
         background_color: '#ffffff',
         display: 'standalone',
         scope: '/',
-        start_url: '/',
+        start_url: '/?v=3',
         icons: [
           {
-            src: 'icons/icon-192x192.png',
+            src: '/icons/Icon_Necse_WhiteTheme_192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: 'icons/icon-512x512.png',
+            src: '/icons/Icon_Necse_WhiteTheme_512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
